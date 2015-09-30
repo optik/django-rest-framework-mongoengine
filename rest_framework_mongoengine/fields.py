ecb3df468ac7dc560f7358ecb346b54bf9bd1e4c
optik <tomek@greymatter.pl>
2015-09-30 12:37:08 +0200

    Py3 compatibility fix for ObjectId

--- rest_framework_mongoengine/fields.py
+++ rest_framework_mongoengine/fields.py
@@ -3,6 +3,7 @@ from django.utils.encoding import smart_str
 from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
+from bson.py3compat import PY3
 from bson.errors import InvalidId
 
 from mongoengine import dereference
@@ -178,8 +179,9 @@ class ObjectIdField(DocumentField):
 
     def to_internal_value(self, data):
         try:
-            return ObjectId(unicode(data))
+            return ObjectId(str(data) if PY3 else unicode(data))
         except Exception as e:
+            print(e)
             raise serializers.ValidationError(e.message)
 
 
